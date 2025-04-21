fc0de7b5d81c5ff2ba294e22b8da0dc229f7ca72
sokiror <okiror1vinald@gmail.com>
2025-04-21 22:29:06 +0300

    change btn content

--- Resources/src/components/Buttons/Buttons.jsx
+++ Resources/src/components/Buttons/Buttons.jsx
@@ -10,11 +10,15 @@ function Buttons() {
     const doubleClick = () => {
         alert("Button double clicked!");
     };
+    const changeContent = (e) => {
+        e.target.innerText = "Clicked!";
+    }
     return (
         <div>
             <button onClick={handleClick}>Click Me</button>
             <button onMouseOver={handleMouseOver}>Hover Over Me</button>
             <button onDoubleClick={doubleClick}>Double Click Me</button>
+            <button onClick={changeContent}>Change Content</button>
         </div>
     )
 }
