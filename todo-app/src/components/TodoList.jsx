4642d3924d1a628968d11f56c1be7a35715be955
sokiror <okiror1vinald@gmail.com>
2025-05-02 21:40:09 +0300

    CSS

---
+++ todo-app/src/components/TodoList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TodoCard from "./TodoCard";
+
+function TodoList() {
+    return (
+        <>
+            <TodoCard />
+            <TodoCard />
+            <TodoCard />
+            <TodoCard />
+        </>
+    );
+}
+
+export default TodoList;
